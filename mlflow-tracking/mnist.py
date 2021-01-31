f0d0d7f058f915216c84e4bcea2da5a87952f48a
wellshs <wellshs@kaist.ac.kr>
2021-01-31 22:24:01 +0900

    Add sklearn autologging

--- mlflow-tracking/mnist.py
+++ mlflow-tracking/mnist.py
@@ -1,7 +1,7 @@
 import mlflow
 from sklearn import datasets, svm
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 mlflow.set_experiment("/scklearn-handwrite")
 
@@ -12,10 +12,10 @@ data = digits.images.reshape((n_samples, -1))
 X_train, X_test, y_train, y_test = train_test_split(
     data, digits.target, test_size=0.5, shuffle=False)
 
+mlflow.sklearn.autolog()
 gamma_list = [10 ** x for x in range(-10, 10)]
 for gamma in gamma_list:
     with mlflow.start_run():
-        mlflow.log_param("gamma", gamma)
         clf = svm.SVC(gamma=gamma)
         clf.fit(X_train, y_train)
         predicted = clf.predict(X_test)
