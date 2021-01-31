b21adf27473704c3008a3fcbfee5f655ed0f959c
wellshs <wellshs@kaist.ac.kr>
2021-01-31 22:03:22 +0900

    mlflow에 여러개의 gamma로 테스트하는 코드 추가

---
+++ mlflow-tracking/mnist.py
@@ -0,0 +1,24 @@
+import mlflow
+from sklearn import datasets, svm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+mlflow.set_experiment("/scklearn-handwrite")
+
+digits = datasets.load_digits()
+
+n_samples = len(digits.images)
+data = digits.images.reshape((n_samples, -1))
+X_train, X_test, y_train, y_test = train_test_split(
+    data, digits.target, test_size=0.5, shuffle=False)
+
+gamma_list = [10 ** x for x in range(-10, 10)]
+for gamma in gamma_list:
+    with mlflow.start_run():
+        mlflow.log_param("gamma", gamma)
+        clf = svm.SVC(gamma=gamma)
+        clf.fit(X_train, y_train)
+        predicted = clf.predict(X_test)
+
+        mlflow.log_metric("accuracy_score", accuracy_score(predicted, y_test))
+
